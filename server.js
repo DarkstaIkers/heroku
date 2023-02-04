fb09d43e3d480a65a32fe9396ee465aced2bcc61
D's <DarkstaIkers@users.noreply.github.com>
2023-02-04 02:37:05 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const corsProxy = require('cors-anywhere');
+
+// Listen on a specific host via the HOST environment variable.
+const host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable.
+const port = process.env.PORT || 1454;
+
+corsProxy.createServer({
+    originWhitelist: [],
+    requireHeader: [],
+    removeHeaders: [
+    'cookie',
+    'cookie2',
+    // Strip Heroku-specific headers
+    'x-request-start',
+    'x-request-id',
+    'via',
+    'connect-time',
+    'total-route-time',
+    'referer',
+    'origin',
+    // Other Heroku added debug headers
+    // 'x-forwarded-for',
+    // 'x-forwarded-proto',
+    // 'x-forwarded-port',
+  ],
+}).listen(port, host, () => {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
